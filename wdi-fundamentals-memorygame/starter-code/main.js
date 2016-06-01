c0d77b425a01f3f32a39cf3ec4d6f7074acf9644
ccasey79 <christiancasey@christians-mbp.home>
2016-06-01 15:10:36 +0100

    added first updates unit 8

---
+++ wdi-fundamentals-memorygame/starter-code/main.js
@@ -0,0 +1,20 @@
+
+// 4 cards with variables
+
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+// using different comparisons on cards
+
+cardOne == cardTwo;
+cardOne == cardFour;
+
+cardOne || cardTwo;
+cardTwo || cardFour;
+
+cardOne != cardTwo;
+cardTwo != cardThree;
+
+
